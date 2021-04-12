266617b7be46e4d09ce72998cff7b12e134a8504
KoAB <dronkoland@gmail.com>
2021-04-12 19:53:53 +0300

    folder renamed

---
+++ TestTreeWebApi/ServiceModels/TreeNodeDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TestTreeWebApi.ApiModels;
+
+namespace TestTreeWebApi.ServiceModels
+{
+    public class TreeNodeDTO
+    {
+        public string Name { get; set; }
+
+        public List<TreeNodeDTO> Childs { get; set; }
+    }
+}
---
+++ TestTreeWebApi/ServiceModels/TreeNodeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTreeWebApi.ServiceModels
+{
+    public class TreeNodeException : ArgumentException
+    {
+        public TreeNodeException(string message)
+            : base(message)
+        { }
+    }
+}
